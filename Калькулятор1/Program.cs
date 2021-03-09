0a50b5ce9791dd40f4908dc71b26fe63d1f6f4f1
Иван Киселев <kiselevia1.20@edu.ystu.ru>
2021-03-09 14:20:05 +0300

    калькулятор1

---
+++ Калькулятор1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Калькулятор1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                double firstValue, secondeValue;
+                string action;
+
+                Console.WriteLine("Введите число 1");
+                firstValue = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите число 2");
+                secondeValue = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Выберите операцию: '+' '-' '*' '/'");
+                action = Console.ReadLine();
+
+                switch (action)
+                {
+                    case "+":
+                        Console.WriteLine(firstValue + secondeValue);
+                        break;
+                    case "-":
+                        Console.WriteLine(firstValue - secondeValue);
+                        break;
+                    case "*":
+                        Console.WriteLine(firstValue * secondeValue);
+                        break;
+                    case "/":
+                        if (secondeValue == 0)
+                        {
+                            Console.WriteLine("На ноль делить нельзя");
+                        }
+                        else
+                        {
+                            Console.WriteLine(firstValue / secondeValue);
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Ошибка! Неправильное действие!");
+                        break;
+
+
+
+                }
+
+                Console.ReadLine();
+
+
+            }
+        }
+    }
+}
